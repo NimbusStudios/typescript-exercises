a962d25e3040c5baec5c39f128b302d6500e2ea7
NimbusStudios <165320227+NimbusStudios@users.noreply.github.com>
2024-06-24 18:53:49 +0200

    Update Excercise 1.ts

---
+++ Excercise 1.ts
@@ -0,0 +1,11 @@
+// 1. Variable with inferred string type
+let inferredString = "Hello, world!";
+
+// 2. Variable with explicitly stated number type
+let explicitNumber: number = 42;
+
+// 3. Variable that can hold a value of any type
+let anyValue: any = "This can be any type";
+
+// 4. Variable that can hold either a string or a number
+let stringOrNumber: string | number = "Hello";
